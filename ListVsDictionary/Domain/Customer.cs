eb4afcab02f58bc8fb22a66ccfd4fb4742e4a1b5
Jeff Zuerlein <Jeff@Zuerlein.com>
2023-12-11 09:19:51 -0600

    Add project files.

---
+++ ListVsDictionary/Domain/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListVsDictionary.Domain
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public int Qty { get; set; }
+        public decimal PriceCharged { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { set; get; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ ListVsDictionary/TestRepository.cs
@@ -0,0 +1,72 @@
+﻿using ListVsDictionary.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListVsDictionary
+{
+    public class TestRepository
+    {
+        private List<Customer> _customers = new List<Customer>();
+        private List<Order> _orders = new List<Order>();
+        private List<OrderItem> _orderItems = new List<OrderItem>();
+        private List<Product> _products = new List<Product>();
+
+        public TestRepository(int numCustomers, int numOrdersPerCustomer, int numOrderItemsPerOrder, int numProducts)
+        {
+            for (int i = 1; i <= numProducts; i++)
+            {
+                _products.Add(new Product() { ProductId = i, Name = i.ToString(), Price = i });
+            }
+
+            for (int i = 1; i <= numCustomers; i++)
+            {
+                _customers.Add(new Customer() { CustomerId = i, Name = i.ToString() });
+            }
+
+            for (int i = 1; i <= numOrdersPerCustomer; i++)
+            {
+                for (int j = 1; i <= numOrdersPerCustomer; j++)
+                {
+                    _orders.Add(new Order() { OrderId = i * j, CustomerId = i, OrderNumber = (i * j).ToString() });
+                }
+            }
+
+            var random = new Random();
+
+            for(int i = 1; i <= numOrdersPerCustomer; i++)
+            {
+                for(int j = 1; i <= numOrdersPerCustomer; j++)
+                {
+                    for(int z = 1; z <= numOrderItemsPerOrder; z++)
+                    {
+                        _orderItems.Add(new OrderItem() { OrderItemId = i * j * z, OrderId = i * j, PriceCharged = i * j * z, Qty = 1, ProductId = random.Next(numProducts) });
+                    }
+                }
+            }
+
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            foreach(var orderItem in _orderItems)
+            {
+                orderItem.Product = _products.First(_ => _.ProductId == orderItem.ProductId);
+            }
+
+            foreach(var order in _orders)
+            {
+                order.OrderItems = _orderItems.Where(_ => _.OrderId == order.OrderId).ToList();
+            }
+
+            foreach (var customer in _customers)
+            {
+                customer.Orders = _orders.Where(_ => _.CustomerId ==  customer.CustomerId).ToList();
+            }
+
+            return _customers;
+        }
+    }
+}
